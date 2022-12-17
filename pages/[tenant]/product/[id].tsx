ba7457593ae4705084ac7fae8ba468ef1e186840
Cleyton Mendes <cleyton.mendest@gmail.com>
2022-12-17 20:18:24 -0300

    feat(pdp)inclui estrutura e css

--- pages/[tenant]/product/[id].tsx
+++ pages/[tenant]/product/[id].tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
 
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { useEffect } from 'react'
+import Button from '../../../components/Button'
 import Header from '../../../components/Header'
 import { useAppContext } from '../../../contexts/AppContext'
 import { useApi } from '../../../libs/useApi'
+import { useFormatter } from '../../../libs/useFormatter'
 import styles from '../../../styles/Product-id.module.css'
 import { Product } from '../../../types/Product'
 import { Tenant } from '../../../types/Tenant'
@@ -13,10 +16,15 @@ import { Tenant } from '../../../types/Tenant'
 
 const Product = (data: Props) => {
     const { tenant, setTenant } = useAppContext()
+    const formatter = useFormatter()
 
     useEffect(() => {
         setTenant(data.tenant)
     }, [data.tenant, setTenant])
+
+    const handleAddToCart = () =>{
+        
+    }
 
     return (
         <div className={styles.container}>
@@ -37,6 +45,30 @@ const Product = (data: Props) => {
 
             <div className={styles.productImage}>
                 <img src={data.product.image} alt="" />
+            </div>
+
+            <div className={styles.category}>{data.product.categoryName}</div>
+            <div className={styles.title} style={{borderBottomColor: data.tenant.primaryColor}}>{data.product.name}</div>
+            <div className={styles.line}></div>
+            <div className={styles.description}>{data.product.description}</div>
+            <div className={styles.qtText}>Quantidade</div>
+            <div className={styles.area}>
+                <div className={styles.areaLeft}>
+
+                </div>
+                <div
+                    className={styles.areaRight}
+                    style={{color: data.tenant.primaryColor}}>
+                    {formatter.formatPrice(data.product.price)}
+                </div>
+            </div>
+            <div className={styles.buttonArea}>
+                <Button 
+                    color={data.tenant.primaryColor}
+                    label="Adicionar a sacola"
+                    onClick={handleAddToCart}
+                    fill
+                />
             </div>
         </div>
     )
